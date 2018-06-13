9c694778eb1a244112a0cf43d46ea63ea396eeb1
binayag1 <binaya.g1@gmail.com>
2018-06-13 14:38:09 +1000

    Use of Synchronized Keyword

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 	Build id: 20180405-1200
 
 # In this Commit: 
-	The default priority of thread is 5 which is normal priority. We can set low and maximum priority by using 
-	value 1 and 10. The priority of thread can be known by using thread.getPriority() and set by using thread.setPriority(). The name of thread can be known by using thread.getName() and set by using thread.setName() OR we can assign name during the creation of thread as shown in example. 
+	A new class has been added called Counter which increment the value by 1. Thread 1 and Thread 2 call the 
+	method of same class. During the call we expect value 2000, as thread 1 has loop of 1000 and thread 2 has
+	1000. But as both may call the method at the same time and fetch the same value and increment. This will 
+	lead to the incorrect result. To avoid such error we need to use synchronized keyword. Which will allow
+	only one threat to execute the method at a time. 
--- src/main/java/binaya/MultiThreading/App.java
+++ src/main/java/binaya/MultiThreading/App.java
@@ -4,45 +4,24 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Counter c=new Counter();
     	Thread t1=new Thread(
     			()->{
-    				for(int i=1;i<=5;i++) {
-    					System.out.println("Hello from Thread 1. Priority: "+Thread.currentThread().getPriority());
-    					try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+    				for(int i=1;i<=1000;i++) {
+    					c.increament();
     				}
     			},"thread1"
     			
     			);
     	Thread t2=new Thread(
     			()->{
-    				for(int i=1;i<=5;i++) {
-    					System.out.println("Hello from Thread 2. Priority: "+Thread.currentThread().getPriority());
-    					try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+    				for(int i=1;i<=1000;i++) {
+    					c.increament();
     				}
     			},"thread2"
     			
     			);	
-    	System.out.println(t1.getName());
-    	System.out.println(t2.getName());
-    	t1.setPriority(1);
-    	t2.setPriority(10);
     	t1.start();
-    	try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
     	t2.start();
     	try {
 			t1.join();
@@ -51,9 +30,13 @@ public class App
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	System.out.println("This is message from Main Thread-->>");
-    	System.out.println(t1.isAlive());
-    	
+    	System.out.println(c.count);
     }
+}
+class Counter{
+	int count;
+	public synchronized void increament() {
+		count++;
+	}
 }
 
