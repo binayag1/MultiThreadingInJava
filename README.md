aff98fae03261397942ff13523e4379cf1741744
binayag1 <binaya.g1@gmail.com>
2018-06-13 15:49:18 +1000

    Inter Thread Communication

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 	Build id: 20180405-1200
 
 # In this Commit: 
-	A new class has been added called Counter which increment the value by 1. Thread 1 and Thread 2 call the 
-	method of same class. During the call we expect value 2000, as thread 1 has loop of 1000 and thread 2 has
-	1000. But as both may call the method at the same time and fetch the same value and increment. This will 
-	lead to the incorrect result. To avoid such error we need to use synchronized keyword. Which will allow
-	only one threat to execute the method at a time. 
+	This is the example of inter thread communication, where two thread are created. One thread works to set
+	the value of number where as other thread works to get the number. Here wait(), is used to pause 
+	another thread until the value is set, similarly wait() is used until the value is get. This means, when number is not set, thread for get will wait and after value is set, set waits for get to get the value.  
+	wait() belongs to Object class and hence sleep() belongs to Thread class. When sleep() is used, a thread 
+	is paused for certain time, but wait() will pause the thread until it is being notified. 
--- src/main/java/binaya/MultiThreading/App.java
+++ src/main/java/binaya/MultiThreading/App.java
@@ -4,39 +4,66 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Counter c=new Counter();
-    	Thread t1=new Thread(
-    			()->{
-    				for(int i=1;i<=1000;i++) {
-    					c.increament();
-    				}
-    			},"thread1"
-    			
-    			);
-    	Thread t2=new Thread(
-    			()->{
-    				for(int i=1;i<=1000;i++) {
-    					c.increament();
-    				}
-    			},"thread2"
-    			
-    			);	
-    	t1.start();
-    	t2.start();
-    	try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	System.out.println(c.count);
+    		Hello hello=new Hello();
+    		new Producer(hello);
+    		new Consumer(hello);
     }
 }
-class Counter{
-	int count;
-	public synchronized void increament() {
-		count++;
+
+class Hello{
+	int number;
+	boolean isSet=false;
+	public synchronized void put(int number) {
+		if(isSet) {
+			try{wait();}catch(Exception e) {}
+		}
+		System.out.println("Set Number:"+number);
+		this.number=number;
+		isSet=true;
+		notify();
+	}
+	public synchronized void get() {
+		while(!isSet)
+		{
+			try {wait();}catch(Exception e) {}
+		}
+		System.out.println("Get Number:"+number+"\n");
+		System.out.println("========================");
+		isSet=false;
+		notify();
 	}
 }
-
+class Producer implements Runnable{
+	
+	Hello h1;
+	public Producer(Hello h1) {
+		this.h1=h1;
+		Thread t1=new Thread(this,"thread 1");
+		t1.start();
+	}
+	@Override
+	public void run() {
+		int i=0;
+		while(true) {
+			h1.put(i++);
+			try{Thread.sleep(1000);} catch(Exception e) {}
+		}
+	}
+	
+}
+class Consumer implements Runnable{
+	Hello h2;
+	public Consumer(Hello h2) {
+		this.h2=h2;
+		Thread t2=new Thread(this,"thread 2");
+		t2.start();
+	}
+	@Override
+	public void run() {
+		while(true) {
+			h2.get();
+			try {Thread.sleep(1000);}catch(Exception e) {}
+		}
+	}
+	
+}
