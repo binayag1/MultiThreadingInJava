bcee9ee971aa092e54a1bd8e36e489f32aeeea02
binayag1 <binaya.g1@gmail.com>
2018-06-13 12:27:38 +1000

    Lambda Expression

--- README.md
+++ README.md
@@ -6,8 +6,6 @@
 	Build id: 20180405-1200
 
 # In this Commit: 
-	In previous example, class Thread was used to achieve multithreading. This time the example has been used 
-	Runnable Interface. Runnable Interface has only one method which is Run. 
-	In Thread class we used, thread.start() method to start thread but this time we will implements 
-	Runnable Interface in both of the class and will create two thread in App class. 
-	But this time we will create object of Runnable and pass that to the Thread. 
+	Since, Runnable Interface has only one method that is Run(). So in this case we can reduce the number of
+	line and write lambda expression. In this example, two class has been replaced with lambda expression. Same task has been done with less number of code. I would recommend to run lambda expression in java 8 to get
+	better result. 
--- src/main/java/binaya/MultiThreading/App.java
+++ src/main/java/binaya/MultiThreading/App.java
@@ -1,17 +1,36 @@
 package binaya.MultiThreading;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-    	B b=new B();
-    	C c=new C();
-    	Thread t1=new Thread(b);
-    	Thread t2=new Thread(c);
+    	Thread t1=new Thread(
+    			()->{
+    				for(int i=1;i<5;i++) {
+    					System.out.println("Hello from Thread 1");
+    					try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+    				}
+    			}
+    			
+    			);
+    	Thread t2=new Thread(
+    			()->{
+    				for(int i=1;i<=5;i++) {
+    					System.out.println("Hello from Thread 2");
+    					try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+    				}
+    			}
+    			
+    			);
     	t1.start();
     	try {
 			Thread.sleep(10);
@@ -20,35 +39,6 @@ public class App
 			e.printStackTrace();
 		}
     	t2.start();
-    	
-        
     }
-}
-class B implements Runnable{
-	public void run() {
-		for (int i = 1; i <=5; i++) {
-			System.out.println("Hello from B");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-}
-class C implements Runnable{
-	public void run() {
-		for (int i = 1; i <=5; i++) {
-			System.out.println("Hello from C");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
 }
 
